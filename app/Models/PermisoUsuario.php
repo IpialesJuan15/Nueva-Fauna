c949aa8a88ac34dad45bd85fd8053d14ecfea0f1
Aldahir Requene <aldairrequene@gmail.com>
2025-01-15 18:30:13 -0500

    c

--- app/Models/PermisoUsuario.php
+++ app/Models/PermisoUsuario.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class PermisoUsuario extends Model
 {
-    //
+    protected $table = 'permisos_usuarios';
+    protected $primaryKey = 'perus_id';
+
+    protected $fillable = ['perus_detalle'];
+
+    public $timestamps = false;
+
+    public function tiposUsuarios()
+    {
+        return $this->hasMany(TipoUsuario::class, 'perus_id', 'perus_id');
+    }
 }
