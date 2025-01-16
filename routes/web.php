9fd2f2c3902993dde222632dbd7535a090954da8
Aldahir Requene <aldairrequene@gmail.com>
2025-01-16 14:20:11 -0500

    c

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/FormInvest', function () {
 });
 
 
-
+//Rutas para registrar una especie:
 Route::post('/especies', [EspecieController::class, 'store'])->name('especies.store');
 Route::put('/especies/editar', [EspecieController::class, 'update'])->name('especies.update');
 Route::post('/especies/buscar', [EspecieController::class, 'search'])->name('especies.search');
@@ -43,14 +43,6 @@ Route::get('/home', function () {
 Route::get('/report', function () {
     return view('report');
 });
-<<<<<<< HEAD
-Route::get('/observador', function () {
-    return view('observador'); // Asegúrate de que el archivo observador.blade.php exista en resources/views
-=======
-
-
-
-
 
 Route::get('/register', function () {
     return view('register');
@@ -58,7 +50,7 @@ Route::get('/register', function () {
 
 Route::post('/register', [UserController::class, 'register']);
 
-
+// Ruta Para recuperar contraseña
 Route::get('/recuperarEmail', function () {
     return view('recuperarEmail');
 >>>>>>> 3980256b1330902501c83864fd5c74a7918b9968
@@ -69,41 +61,11 @@ Route::get('/report', function () {
     return view('report'); // Esto carga el archivo resources/views/report.blade.php
 });
 
-<<<<<<< HEAD
-Route::get('/taxonomo', function () {
-    return view('taxonomo');
-})->name('taxonomo');
-=======
 // Ruta principal de prueba
 Route::get('/', function () {
     return "HOLA MUNDO"; // Esto carga el archivo resources/views/welcome.blade.php
 });
->>>>>>> 3980256b1330902501c83864fd5c74a7918b9968
 
 Route::get('/home', function () {
     return view('home');
 });
-Route::get('/login', function () {
-    return view('login'); // Muestra login.blade.php
-})->name('login');
-
-// Ruta para registro de usuarios
-Route::get('/register', function () {
-    return view('register'); // Muestra register.blade.php
-})->name('register');
-
-// Ruta para la página principal
-Route::get('/home', function () {
-    return view('home'); // Muestra home.blade.php
-})->name('home');
-Route::get('/recuperarEmail', function () {
-    return view('recuperarEmail');
-})->name('recuperarEmail');
-Route::get('/registros', function () {
-    return view('registros'); // Cambia "registros" al nombre correcto del archivo Blade.
-})->name('registros');
-
-Route::post('/logout', function () {
-    // Implementa la lógica de cierre de sesión aquí.
-    // Por ejemplo: Auth::logout(); return redirect('/');
-})->name('logout');
